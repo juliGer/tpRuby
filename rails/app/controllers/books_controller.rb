86ebeaa28003cc4ba78c89baa21386a3e401bcf1
juliGer <juuliiger@gmail.com>
2021-01-12 19:56:07 -0300

    agrego devise

--- rails/app/controllers/books_controller.rb
+++ rails/app/controllers/books_controller.rb
@@ -49,7 +49,7 @@ class BooksController < ApplicationController
   private
 
     def set_user
-      @user = User.find(params[:user_id])
+      @user = current_user
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_book
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_155412) do
+ActiveRecord::Schema.define(version: 2021_01_12_214116) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -32,13 +32,17 @@ ActiveRecord::Schema.define(version: 2021_01_09_155412) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", limit: 200, null: false
-    t.string "password_digest", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "books", "users"
+  add_foreign_key "books", "users", on_delete: :cascade
   add_foreign_key "notes", "books"
 end
