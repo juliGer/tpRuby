17b33409369ae9553dc1229a654c5051c5bab96f
Julian <juuliiger@gmail.com>
2021-02-04 22:46:55 -0300

    arreglo registrations

--- rails/app/controllers/registrations_controller.rb
+++ rails/app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
     def create
       super
-      current_user.books.create(user_id: current_user.id,name:"cuaderno_global")
+      if current_user
+        current_user.books.create(user_id: current_user.id,name:"cuaderno_global")
+      end
     end
   end 
